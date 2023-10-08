a163dfb0a455e80df49476823bde76941bbcb22c
itsgreatness <dzhao650@gmail.com>
2023-10-08 16:42:05 -0500

    First bits

--- main.js
+++ main.js
@@ -1,7 +1,30 @@
 var DavidAbilities = {
+    Expression: class Expression {
+
+    },
     Equation: class Equation {
         constructor(leftSide, rightSide) {
-            
+            [this.leftSide, this.rightSide] = [leftSide, rightSide];
+        }
+        add(eq) {
+            return []
+        }
+        get neg() {
+
+        }
+        mult(eq) {
+            /* Multiply by value:
+            this.mult([v,v]);
+            since for any v:
+            v=v
+            is a true equation
+            Same for add
+            neg() shorthand for mult([-1,-1]);
+             */
+            if (eq instanceof Number) {
+                let v;
+                eq = new Equation(v = new Expression(eq), v);
+            }
         }
     }
 }
